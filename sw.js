70cde860b7f50becf00d11e49ed71feafc60766a
natanlimamoura-maker <natan.limamoura@gmail.com>
2026-01-09 19:46:43 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,18 @@
-const cacheName = 'clarity-v1';
-const assets = ['./', './index.html', './manifest.json'];
+const CACHE_NAME = 'clarity-v13';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json'
+];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
 });
